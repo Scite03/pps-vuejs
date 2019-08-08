d2ace44090127d55be4c05c4ae17bc1a709a63d4
Kritsana Wonglomnil <k.wonglomnil@gmail.com>
2019-08-08 09:44:11 +0700

    commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1 class="tital">Navbar</h1>
+        <p>
+            <RouterLink to="/">One</RouterLink>
+        </p>
+        <p>
+            <RouterLink to="/about">Two</RouterLink>
+        </p>
+        <p>
+            <RouterLink to="/three">Three</RouterLink>
+        </p>
+        <p>
+            <RouterLink to="/user">User</RouterLink>
+        </p>
+    </div>
+</template>
---
+++ src/pages/User.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <h1 class="title">Title</h1>
+        <button @click="getUsers()" class="button">Get Users</button>
+        <div class="table-container">
+            <table class="table is-striped">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>UserName</th>
+                        <th>Email</th>
+                        <th>Website</th>
+                    </tr>
+                </thead>
+                <tr v-for="each in users" :key="each.id">
+                    <td>{{each.id}}</td>
+                    <td><RouterLink :to="'/user/' + each.id">{{each.name}}</RouterLink></td>
+                    <td>{{each.username}}</td>
+                    <td>{{each.email}}</td>
+                    <td>{{each.website}}</td>
+                </tr>
+            </table>
+        </div>
+        <!-- <p v-for="each in users" :key="each.id">{{ each.name }}</p>
+        <pre>{{users}}</pre> -->
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+const url = 'https://jsonplaceholder.typicode.com/users'
+export default {
+    data() {
+        return {
+            users: []
+        }
+    },
+    methods: {
+        getUsers(){
+            axios.get(url).then(res => {
+                this.users = res.data
+            })
+        }
+    },
+}
+</script>
